0a8b4e974537ddf90b56968713d70b17b345dd67
WeichaoLi <1303169452@qq.com>
2016-01-06 16:57:02 +0800

    gg

--- HealthKitDemo/ViewController.m
+++ HealthKitDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <HealthKit/HealthKit.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    HKHealthStore *healthStore;
+}
 
 @end
 
@@ -17,6 +20,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    if ([HKHealthStore isHealthDataAvailable]) {
+        NSLog(@"可以使用healthKit");
+        
+        healthStore = [[HKHealthStore alloc] init];
+        
+        NSString *unitIdentifier = HKQuantityTypeIdentifierStepCount;
+        
+        NSSet *sampleTypes = [NSSet setWithObjects:[HKWorkoutType workoutType],
+                              [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierActiveEnergyBurned],
+                              [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning],
+                              [HKQuantityType quantityTypeForIdentifier:unitIdentifier],
+                              nil];
+        
+        [healthStore requestAuthorizationToShareTypes:sampleTypes
+                                            readTypes:nil
+                                           completion:^(BOOL success, NSError * _Nullable error) {
+                                               if (!success) {
+                                                   NSLog(@"===%@", error);
+                                               }
+                                           }];
+        
+        HKQuantity *quantity = [HKQuantity quantityWithUnit:[HKUnit countUnit] doubleValue:(double)1000];
+        HKQuantitySample *quantitySample = [HKQuantitySample quantitySampleWithType:[HKObjectType quantityTypeForIdentifier:unitIdentifier]
+                                                                           quantity:quantity
+                                                                          startDate:[NSDate date]
+                                                                            endDate:[NSDate date]];
+        
+        [healthStore saveObject:quantitySample withCompletion:^(BOOL success, NSError * _Nullable error) {
+            if (success) {
+                NSLog(@"更新步数成功");
+            }
+        }];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
